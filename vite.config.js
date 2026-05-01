e48344774019ac103c843302d8f56fd904ee1cd3
pawan chauhan <pawan.c@lastmilecare.in>
2026-05-01 16:10:36 +0530

    Config changes

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,16 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+
   server: {
-    port: 5173
+    host: '0.0.0.0', // important for external access (dev)
+    port: 5173,
+  },
+
+  preview: {
+    host: '0.0.0.0', // required
+    port: 8002,
+    allowedHosts: ['test1.lastmilecare.in'],
   },
 })
